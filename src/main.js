8e2caa81b7951af122a72444b5b1824d2e29c2a6
mlyrmdhnn <mulyaranadhan@gmail.com>
2025-11-11 22:11:46 +0700

    build

--- src/main.js
+++ src/main.js
@@ -2,14 +2,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
 const app = createApp(App)
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000/api'
+axios.defaults.baseURL = 'https://mlyrmdhnn.my.id/taskflow/api/'
 
 app.use(createPinia())
 app.use(router)
